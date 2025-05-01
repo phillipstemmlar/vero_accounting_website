4e9d12af7103151c3471ebbd93bbeaba580840cf
Phillip Stemmlar Schulze <49020766+phillipstemmlar@users.noreply.github.com>
2025-05-01 13:36:29 +0200

    Updated social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
 
+import SVG from './svg/SVG'
+
 const Footer = () => {
     return (
         <>
@@ -123,35 +125,36 @@ const Footer = () => {
                             <div className="text-md font-medium mb-6">Follow us on social media.</div>
                             <div className="mx-auto text-center mt-2">
                                 <ul className="flex justify-center mb-4 md:mb-0">
-                                    <li>
-                                        <Link
-                                            to="#"
-                                            className="flex justify-center items-center fg-primary hover:text-gray-500 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
-                                            aria-label="Twitter"
+                                    <li className="mr-5">
+                                        <a
+                                            href="https://www.linkedin.com/"
+                                            target="_blank"
+                                            className="flex justify-center items-center fg-primary hover:text-gray-500 bg-white hover:bg-white-100 rounded-full shadow transition duration-50 ease-in-out"
+                                            aria-label="LinkedIn"
                                         >
-                                            <svg
-                                                className="w-8 h-8 fill-current"
-                                                viewBox="0 0 32 32"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                                <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
-                                            </svg>
-                                        </Link>
+                                            {SVG.linkedIn}
+                                        </a>
                                     </li>
-                                    <li className="ml-4">
-                                        <Link
-                                            to="#"
-                                            className="flex justify-center items-center fg-primary hover:text-gray-500 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+                                    <li className="mr-5">
+                                        <a
+                                            href="https://www.facebook.com/"
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="flex justify-center items-center fg-primary hover:text-gray-500 bg-white hover:bg-white-100 rounded-full shadow transition duration-50 ease-in-out"
                                             aria-label="Facebook"
                                         >
-                                            <svg
-                                                className="w-8 h-8 fill-current"
-                                                viewBox="0 0 32 32"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                                <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
-                                            </svg>
-                                        </Link>
+                                            {SVG.facebook}
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="https://www.instagram.com/"
+                                            target="_blank"
+                                            className="flex justify-center items-center fg-primary hover:text-gray-500 bg-white hover:bg-white-100 rounded-full shadow transition duration-50 ease-in-out"
+                                            aria-label="Instagram"
+                                        >
+                                            {SVG.instagram}
+                                        </a>
                                     </li>
                                 </ul>
                             </div>
@@ -164,7 +167,7 @@ const Footer = () => {
                                 Copyright &copy; {new Date().getFullYear()}
                                 {'  '}
                                 <HashLink to="#" className=" hover:text-gray-900">
-                                    Molad e Konsult
+                                    Vero Accounting and Advisory
                                 </HashLink>
                                 . All rights reserved.
                             </div>
---
+++ src/components/svg/SVG.js
@@ -0,0 +1,11 @@
+import facebook from './facebook-svg'
+import instagram from './instagram-svg'
+import linkedIn from './linkedIn-svg'
+import twitter from './twitter-svg'
+
+export default {
+    facebook,
+    instagram,
+    linkedIn,
+    twitter,
+}
